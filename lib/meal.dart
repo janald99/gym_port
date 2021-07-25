c831f319dfc9aef8aa8b47f3073e804631dbb1d3
janald99 <janald99@hotmail.com>
2021-07-25 23:07:23 +0800

    Another commit

---
+++ lib/meal.dart
@@ -0,0 +1,59 @@
+class Meal {
+  final String mealTime, name, imagePath, kiloCaloriesBurnt, timeTaken;
+  final String preparation;
+  final List ingredients;
+
+  Meal(
+      {required this.mealTime,
+      required this.name,
+      required this.imagePath,
+      required this.kiloCaloriesBurnt,
+      required this.timeTaken,
+      required this.preparation,
+      required this.ingredients});
+}
+
+final meals = [
+  Meal(
+      mealTime: "BREAKFAST",
+      name: "Fruit Granola",
+      kiloCaloriesBurnt: "271",
+      timeTaken: "10",
+      imagePath: "assets/fruit_granola.jpg",
+      ingredients: [
+        "1 cup of granola",
+        "1 banana",
+        "1/2 cup of raisins",
+        "1 tbsp of honey",
+      ],
+      preparation:
+          '''resgg.'''),
+  Meal(
+      mealTime: "DINNER",
+      name: "Pesto Pasta",
+      kiloCaloriesBurnt: "612",
+      timeTaken: "15",
+      imagePath: "assets/pesto_pasta.jpg",
+      ingredients: [
+        "1 cup of granola",
+        "1 banana",
+        "1/2 cup of raisins",
+        "1 tbsp of honey",
+      ],
+      preparation:
+          '''Loremtts.'''),
+  Meal(
+      mealTime: "SNACK",
+      name: "Keto Snack",
+      kiloCaloriesBurnt: "414",
+      timeTaken: "16",
+      imagePath: "assets/keto_snack.jpg",
+      ingredients: [
+        "1 cup of granola",
+        "1 banana",
+        "1/2 cup of raisins",
+        "1 tbsp of honey",
+      ],
+      preparation:
+          '''Lor turpis.'''),
+];
